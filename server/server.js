9aeeba3f853556de69fb508b02f2006919178a01
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-25 10:55:16 -0400

    Conditional routes for type of API used.

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
 //If using apolloServer, don't use routes.
 if (apolloServer){
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, ''../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
 else 
